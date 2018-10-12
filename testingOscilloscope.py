7ad9b4499f46ca3f21c1e21604a0f8d6a14b5199
dillancellier <cellierdillan@gmail.com>
2018-10-12 16:47:55 -0500

    triggers

---
+++ testingOscilloscope.py
@@ -0,0 +1,229 @@
+from psychopy import visual, core
+win = visual.Window([1680,1050],color=(0,0,0),colorSpace='rgb',units='deg',monitor='testMonitor',fullscr=False)
+
+import numpy as np  # whole numpy lib is available, prepend 'np.'
+from numpy import (sin, cos, tan, log, log10, pi, average,
+                   sqrt, std, deg2rad, rad2deg, linspace, asarray)
+import serial
+
+port=serial.Serial('COM4',baudrate=115200) # based on the biosemi website-- may be wrong?
+vis_deg=2
+n_trials=200
+no_stim=12
+if no_stim==12:
+
+    
+
+    noon = visual.Circle(
+
+        win=win, name='12',
+
+        size=(0.60, 0.60),
+
+        ori=0, pos=(0, vis_deg),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=0.0, interpolate=True)
+
+    noon.setAutoDraw(True)
+
+    one_oclock = visual.Circle(
+
+        win=win, name='1',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=((vis_deg/2), (sqrt(3)/2)*vis_deg),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-1.0, interpolate=True)
+
+    one_oclock.setAutoDraw(True)
+
+    two_oclock = visual.Circle(
+
+        win=win, name='2',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=(((sqrt(3)/2)*vis_deg), (vis_deg/2)),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-2.0, interpolate=True)
+
+    two_oclock.setAutoDraw(True)
+
+    three_oclock = visual.Circle(
+
+        win=win, name='3',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=(vis_deg, 0),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-3.0, interpolate=True)
+
+    three_oclock.setAutoDraw(True)
+
+    four_oclock = visual.Circle(
+
+        win=win, name='4',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=(((sqrt(3)/2)*vis_deg), -(vis_deg/2)),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-4.0, interpolate=True)
+
+    four_oclock.setAutoDraw(True)
+
+    five_oclock = visual.Circle(
+
+        win=win, name='5',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=((vis_deg/2), -((sqrt(3)/2)*vis_deg)),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-5.0, interpolate=True)
+
+    five_oclock.setAutoDraw(True)
+
+    six_oclock = visual.Circle(
+
+        win=win, name= '6', 
+
+        size=(.09,0.15),  
+
+        lineWidth=7, lineColor=None, fillColor=None,
+
+        pos=(0.0,-2))
+
+    six_oclock.setAutoDraw(True)
+
+    eleven_oclock = visual.Circle(
+
+        win=win, name='11',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=(-1, ((sqrt(3)/2)*2)),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-1.0, interpolate=True)
+
+    eleven_oclock.setAutoDraw(True)
+
+    ten_oclock = visual.Circle(
+
+        win=win, name='10',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=(-((sqrt(3)/2)*vis_deg), (vis_deg/2)),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-2.0, interpolate=True)
+
+    ten_oclock.setAutoDraw(True)
+
+    nine_oclock = visual.Circle(
+
+        win=win, name='9',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=(-vis_deg, 0),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-3.0, interpolate=True)
+
+    nine_oclock.setAutoDraw(True)
+
+    eight_oclock = visual.Circle(
+
+        win=win, name='8',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=(-((sqrt(3)/2)*vis_deg), -(vis_deg/2)),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-4.0, interpolate=True)
+
+    eight_oclock.setAutoDraw(True)
+
+    seven_oclock = visual.Circle(
+
+        win=win, name='7',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=(-(vis_deg/2), -((sqrt(3)/2)*vis_deg)),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-5.0, interpolate=True)
+
+    seven_oclock.setAutoDraw(True)
+
+    stimuli=[one_oclock,two_oclock,three_oclock,four_oclock,five_oclock,six_oclock,seven_oclock,eight_oclock,nine_oclock,ten_oclock,eleven_oclock,noon]
+
+for stim in stimuli:
+
+    stim.size=(0.70,0.70)
+    stim.setLineColor([1,1,1])
+    stim.setFillColor
+#distractor_stim.size=([.5,.5])
+#target_stim.size=([.5,.5])
+
+for n in range(n_trials):
+    for stim in stimuli:
+        stim.opacity=0
+    win.flip()
+    core.wait(.5)
+    for stim in stimuli:
+        stim.opacity=1
+    port.close()
+    port.open()
+    port.write(bytes([255]))
+    port.flush()
+    #core.wait(.2)
+    #port.close()
+    win.flip()
+    core.wait(.5)
