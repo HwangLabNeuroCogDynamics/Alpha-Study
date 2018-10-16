f597fc86c378f63b6f3f61ff9e06a7d1af5f2bee
dillancellier <cellierdillan@gmail.com>
2018-10-16 15:55:56 -0500

    change # of circles

--- alpha_code_USETHIS.py
+++ alpha_code_USETHIS.py
@@ -30,7 +30,7 @@ os.chdir(_thisDir)
 
 expName = 'alpha_pilot'  # from the Builder filename that created this script
 
-expInfo = {'subject': '', 'session': '01','f or b?':'f','no stim':'12','t in d?':'n', 'lat?':'n','COMPUTER (b,e,d,m)':'b'}
+expInfo = {'subject': '', 'session': '01','f or b?':'f','no stim':'10','t in d?':'n', 'lat?':'n','COMPUTER (b,e,d,m)':'b'}
 
 # where 'f or b?' is flexible v blocked. blocked is default
 
@@ -54,7 +54,7 @@ expInfo['expName'] = expName
 
 from psychopy import visual, core
 
-win = visual.Window([1680,1050],units='deg',fullscr=True,monitor='testMonitor')
+win = visual.Window([1680,1050],units='deg',fullscr=False,monitor='testMonitor')
 
 # ###############Flags####################################################################################
 
@@ -108,22 +108,25 @@ else:
 # # MAKE SURE PATH TO STIMULI IS THE RIGHT ONE FOR THE COMPUTER YOU'RE USING
 
 
-if expInfo['COMPUTER (b,e,d,m)']=='b':
-    target_stim=visual.ImageStim(win, image='C:\Stimuli\T2.png') #size=([.5,.5]
+if expInfo['COMPUTER (b,e,d,m)']=='b': 
+    target_stim=visual.ImageStim(win, image='C:\Stimuli\T2.png') 
     distractor_stim=visual.ImageStim(win, image='C:\Stimuli\I3.png') #behavioral stimulus presentation Dell
-    filename='C:/psychopyData'+u'/%s_%s_%s_%s' % (expInfo['subject'], expName, expInfo['session'],expInfo['date'])
+    filename='Z:/psychopyData'+u'/%s_%s_%s_%s' % (expInfo['subject'], expName, expInfo['session'],expInfo['date'])
     refresh_rate=50
 elif expInfo['COMPUTER (b,e,d,m)']=='d':
     target_stim=visual.ImageStim(win, image='C:\\Users\\dillc\\Downloads\\T2.png')
     distractor_stim=visual.ImageStim(win, image='C:\\Users\\dillc\\Downloads\\I3.png') #dillan's computer
     filename='C:/Users/dillc/Documents'+u'/%s_%s_%s_%s' % (expInfo['subject'], expName, expInfo['session'],expInfo['date'])
+    refresh_rate=50
 elif expInfo['COMPUTER (b,e,d,m)']=='m':
     target_stim=visual.ImageStim(win, image='/Users/dcellier/Documents/GitHub/Alpha-Study/stimuli/T2.png') 
     distractor_stim=visual.ImageStim(win, image='/Users/dcellier/Documents/GitHub/Alpha-Study/stimuli/I3.png')
     filename='/Users/Shared/'+u'data/%s_%s_%s_%s' % (expInfo['subject'], expName, expInfo['session'],expInfo['date'])
-#elif expInfo['COMPUTER (b,e,d,m)']=='e':
-    #target_stim=visual.ImageStim(win, image='/Users/dcellier/Documents/GitHub/Alpha-Study/stimuli/T2.png') ??????
-    #distractor_stim=visual.ImageStim(win, image='/Users/dcellier/Documents/GitHub/Alpha-Study/stimuli/I3.png') ?????
+elif expInfo['COMPUTER (b,e,d,m)']=='e':
+    target_stim=visual.ImageStim(win, image='C:\Stimuli\T2.png') 
+    distractor_stim=visual.ImageStim(win, image='C:\Stimuli\I3.png') #behavioral stimulus presentation Dell
+    filename='C:/psychopyData'+u'/%s_%s_%s_%s' % (expInfo['subject'], expName, expInfo['session'],expInfo['date'])
+    refresh_rate=50
 else:
     # Data file name stem = absolute path + name; later add .psyexp, .csv, .log, etc
     filename = _thisDir + os.sep + u'data/%s_%s_%s_%s' % (expInfo['subject'], expName, expInfo['session'],expInfo['date'])
@@ -151,7 +154,7 @@ print(stimList)
 cue_type_reps=len(cue_valid)*num_reps #the number of times that the 'target' or 'distractor' cue types should be repeated is equal to the number of validity types (i.e. 2 per cue type) times the n repeats for each condition (ie, 3)
 
 EEGflag=0
-if expInfo['COMPUTER (b,e,d,m)']=='b':
+if expInfo['COMPUTER (b,e,d,m)']=='e':
     EEGflag=1
     #added trigs
     delay1trig=bytes([101])
@@ -169,7 +172,8 @@ if expInfo['COMPUTER (b,e,d,m)']=='b':
 # ####stimulus##############################################################################################
 
 def wait_here(t):
-    num_frames=int(refresh_rate*t)
+    interval=1/refresh_rate
+    num_frames=int(t/interval)
     for n in range(num_frames):
         draw_fixation()
         win.flip()
@@ -183,7 +187,7 @@ def pracCond(thisBlock,n_practrials=1):
     pracDataList=[]
     for n in range(n_practrials):
     
-        ITI=np.random.uniform(3.8,4.5)
+        ITI=make_ITI('b')
 
         # info for this block --for subject ######################################
 
@@ -224,40 +228,30 @@ def pracCond(thisBlock,n_practrials=1):
 
                 # randomly select two circles to cue
 
-                cue_target_1=np.random.choice(stimuli[:6],1)
+                cue_target_1=np.random.choice(right_stim,1)
 
-                cue_target_2=np.random.choice(stimuli[6:],1)
+                cue_target_2=np.random.choice(left_stim,1)
 
         else: #otherwise, select a new circle every time
 
-            cue_target_1=np.random.choice(stimuli[:6],1)
+            cue_target_1=np.random.choice(right_stim,1)
 
-            cue_target_2=np.random.choice(stimuli[6:],1)
+            cue_target_2=np.random.choice(left_stim,1)
 
 
 
         #ensure that cue'd circles don't end up too close to each other, ie at clock positions 12 and 1, or 6 and 7
-
-        while ((cue_target_1[0] is noon)and(cue_target_2[0] is one_oclock)) or ((cue_target_1[0] is six_oclock) and (cue_target_2[0] is seven_oclock)):
-
-            cue_target_1=np.random.choice(stimuli[:6],1)
-
-            cue_target_2=np.random.choice(stimuli[6:],1)
-
-
-
-        if thisBlock['cue']=='target':
-            cue_target_1[0].setLineColor([-1,1,-1])
+        if no_stim==12:
+            while ((cue_target_1[0] is noon)and(cue_target_2[0] is one_oclock)) or ((cue_target_1[0] is six_oclock) and (cue_target_2[0] is seven_oclock)):
     
-            cue_target_2[0].setLineColor([-1,1,-1])
-        elif thisBlock['cue']=='distractor': # some other color than green. blue?
-            cue_target_1[0].setLineColor([0,0,1])
+                cue_target_1=np.random.choice(stimuli[:6],1)
     
-            cue_target_2[0].setLineColor([0,0,1])
-        elif thisBlock['cue']=='neutral': # yellow?
-            cue_target_1[0].setLineColor([1,1,0])
-    
-            cue_target_2[0].setLineColor([1,1,0])
+                cue_target_2=np.random.choice(stimuli[6:],1)
+
+
+        color_ind=cue_types_scramble.index(thisBlock['cue'])
+        cue_target_1[0].setLineColor(colors_scramble[color_ind])
+        cue_target_2[0].setLineColor(colors_scramble[color_ind])
     
 
         draw_fixation()
@@ -288,13 +282,13 @@ def pracCond(thisBlock,n_practrials=1):
 
         
 
-        target_loc1 = list(stimuli[:6]).index(cue_target_1[0]) #where are the two cued circles?
+        target_loc1 = list(right_stim).index(cue_target_1[0]) #where are the two cued circles?
 
-        stim_minus_one = np.delete(stimuli[:6], target_loc1) 
+        stim_minus_one = np.delete(right_stim, target_loc1) 
 
-        target_loc2 = list(stimuli[6:]).index(cue_target_2[0]) 
+        target_loc2 = list(left_stim).index(cue_target_2[0]) 
 
-        stim_minus_two = np.delete(stimuli[6:],target_loc2) #get a list of stimuli that don't include cued circles
+        stim_minus_two = np.delete(left_stim,target_loc2) #get a list of stimuli that don't include cued circles
 
         
 
@@ -348,11 +342,11 @@ def pracCond(thisBlock,n_practrials=1):
 
                 if cue_side=='L': #if the target is on the Left side of the clock, we want the dist on the right and vice versa
 
-                    distractor_stim.pos=(np.random.choice(stimuli[:6],1))[0].pos
+                    distractor_stim.pos=(np.random.choice(right_stim,1))[0].pos
 
                 else:
 
-                    distractor_stim.pos= (np.random.choice(stimuli[6:],1))[0].pos
+                    distractor_stim.pos= (np.random.choice(left_stim,1))[0].pos
 
             
 
@@ -378,11 +372,11 @@ def pracCond(thisBlock,n_practrials=1):
 
                 if cue_side=='L': #if the dist is on the Left side of the clock, we want the target on the right and vice versa
 
-                    target_stim.pos=(np.random.choice(stimuli[:6],1))[0].pos
+                    target_stim.pos=(np.random.choice(right_stim,1))[0].pos
 
                 else:
 
-                    target_stim.pos= (np.random.choice(stimuli[6:],1))[0].pos
+                    target_stim.pos= (np.random.choice(left_stim,1))[0].pos
 
             
 
@@ -575,9 +569,9 @@ def make_csv(filename):
 
 def make_ITI(exp_type):
     if exp_type=='b' or exp_type=='m' or exp_type=='d':
-        ITI=np.random.choice([1.5,2.0,2.5],1)[0] #averagaes to around 2 second
+        ITI=np.random.choice([1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.1,2.2,2.3,2.4,2.5,2.6],1)[0] #averages to around 2 second?
     elif exp_type=='e':
-        ITI=np.random.uniform([3.5,4,4.5],1)[0] # averages to around 4 seconds
+        ITI=np.random.choice([3.4,3.5,3.6,3.7,3.8,3.9,4,4.1,4.2,4.3,4.4,4.5,4.6],1)[0] # averages to around 4 seconds?
     return ITI
 
 intro_msg= visual.TextStim(win, pos=[0, .5],units='norm', text='Welcome to the experiment!')
@@ -815,6 +809,174 @@ if no_stim==12:
     seven_oclock.setAutoDraw(True)
 
     stimuli=[one_oclock,two_oclock,three_oclock,four_oclock,five_oclock,six_oclock,seven_oclock,eight_oclock,nine_oclock,ten_oclock,eleven_oclock,noon]
+    right_stim=stimuli[:6]
+    left_stim=stimuli[6:]
+
+if no_stim==10:
+
+    one_oclock = visual.Circle(
+
+        win=win, name='1',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=((vis_deg/2), (sqrt(3)/2)*vis_deg),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-1.0, interpolate=True)
+
+    one_oclock.setAutoDraw(True)
+
+    two_oclock = visual.Circle(
+
+        win=win, name='2',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=(((sqrt(3)/2)*vis_deg), (vis_deg/2)),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-2.0, interpolate=True)
+
+    two_oclock.setAutoDraw(True)
+
+    three_oclock = visual.Circle(
+
+        win=win, name='3',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=(vis_deg, 0),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-3.0, interpolate=True)
+
+    three_oclock.setAutoDraw(True)
+
+    four_oclock = visual.Circle(
+
+        win=win, name='4',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=(((sqrt(3)/2)*vis_deg), -(vis_deg/2)),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-4.0, interpolate=True)
+
+    four_oclock.setAutoDraw(True)
+
+    five_oclock = visual.Circle(
+
+        win=win, name='5',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=((vis_deg/2), -((sqrt(3)/2)*vis_deg)),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-5.0, interpolate=True)
+
+    five_oclock.setAutoDraw(True)
+
+    eleven_oclock = visual.Circle(
+
+        win=win, name='11',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=(-(vis_deg/2), ((sqrt(3)/2)*vis_deg)),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-1.0, interpolate=True)
+
+    eleven_oclock.setAutoDraw(True)
+
+    ten_oclock = visual.Circle(
+
+        win=win, name='10',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=(-((sqrt(3)/2)*vis_deg), (vis_deg/2)),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-2.0, interpolate=True)
+
+    ten_oclock.setAutoDraw(True)
+
+    nine_oclock = visual.Circle(
+
+        win=win, name='9',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=(-vis_deg, 0),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-3.0, interpolate=True)
+
+    nine_oclock.setAutoDraw(True)
+
+    eight_oclock = visual.Circle(
+
+        win=win, name='8',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=(-((sqrt(3)/2)*vis_deg), -(vis_deg/2)),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-4.0, interpolate=True)
+
+    eight_oclock.setAutoDraw(True)
+
+    seven_oclock = visual.Circle(
+
+        win=win, name='7',
+
+        size=(0.09, 0.15),
+
+        ori=0, pos=(-(vis_deg/2), -((sqrt(3)/2)*vis_deg)),
+
+        lineWidth=7, lineColor=None, lineColorSpace='rgb',
+
+        fillColor=None, fillColorSpace='rgb',
+
+        opacity=1, depth=-5.0, interpolate=True)
+
+    seven_oclock.setAutoDraw(True)
+
+    stimuli=[one_oclock,two_oclock,three_oclock,four_oclock,five_oclock,seven_oclock,eight_oclock,nine_oclock,ten_oclock,eleven_oclock]
+    right_stim=stimuli[:5]
+    left_stim=stimuli[5:]
 
 for stim in stimuli:
 
@@ -837,11 +999,15 @@ blocks={}
 #we want to initialize another list of cues wherein the order is randomized so that target blocks or neutral or distractor blocks come first, counterbalanced over subjects
 order=np.random.choice((len(cue_types)+1),(len(cue_types)+1),replace=False) # num of conds in cue_types +1 for the neutral cue = 3  #this is randomly coming up with the indices of the conds in the scrambled list
 cue_types_scramble=np.zeros(((len(cue_types)+1))) 
+colors_scramble=np.zeros((3))
 cues=['neutral']+cue_types #yields ['neutral','target','distractor'] so that the following for loop can select these and insert them into cue_types_scramble
+colors=([-1,1,-1],[0,0,1],[1,1,0])
 order=list(order)
 cue_types_scramble=list(cue_types_scramble)
+colors_scramble=list(colors_scramble)
 for ind in order:
     cue_types_scramble[ind]=cues[order.index(ind)]
+    colors_scramble[ind]=colors[order.index(ind)]
 
 print(cue_types_scramble)
 k=0
@@ -969,37 +1135,40 @@ for cue in cue_types_scramble: #looping through the types of cues in sequence, s
     
                     # randomly select two circles to cue
     
-                    cue_target_1=np.random.choice(stimuli[:6],1) # SORRY that I named these stupidly-- the cues are not always cueing the target!
+                    cue_target_1=np.random.choice(right_stim,1) # SORRY that I named these stupidly-- the cues are not always cueing the target!
     
-                    cue_target_2=np.random.choice(stimuli[6:],1)
+                    cue_target_2=np.random.choice(left_stim,1)
     
             else: #otherwise, select a new circle every time
     
-                cue_target_1=np.random.choice(stimuli[:6],1)
+                cue_target_1=np.random.choice(right_stim,1)
     
-                cue_target_2=np.random.choice(stimuli[6:],1)
+                cue_target_2=np.random.choice(left_stim,1)
 
             #ensure that cue'd circles don't end up too close to each other, ie at clock positions 12 and 1, or 6 and 7
-    
-            while (((cue_target_1[0] is noon)and(cue_target_2[0] is one_oclock)) or ((cue_target_2[0] is noon)and(cue_target_1[0] is one_oclock))) or (((cue_target_1[0] is six_oclock)and(cue_target_2[0] is seven_oclock)) or ((cue_target_2[0] is six_oclock)and(cue_target_1[0] is seven_oclock))):
-    
-                cue_target_1=np.random.choice(stimuli[:6],1)
-    
-                cue_target_2=np.random.choice(stimuli[6:],1)
-    
-    
-            if thisBlock['cue']=='target':
-                cue_target_1[0].setLineColor([-1,1,-1])
+            if no_stim==12:
+                while (((cue_target_1[0] is noon)and(cue_target_2[0] is one_oclock)) or ((cue_target_2[0] is noon)and(cue_target_1[0] is one_oclock))) or (((cue_target_1[0] is six_oclock)and(cue_target_2[0] is seven_oclock)) or ((cue_target_2[0] is six_oclock)and(cue_target_1[0] is seven_oclock))):
         
-                cue_target_2[0].setLineColor([-1,1,-1])
-            elif thisBlock['cue']=='distractor': # some other color than green. blue?
-                cue_target_1[0].setLineColor([0,0,1])
+                    cue_target_1=np.random.choice(right_stim,1)
         
-                cue_target_2[0].setLineColor([0,0,1])
-            elif thisBlock['cue']=='neutral': # yellow?
-                cue_target_1[0].setLineColor([1,1,0])
-        
-                cue_target_2[0].setLineColor([1,1,0])
+                    cue_target_2=np.random.choice(left_stim,1)
+    
+            color_ind=cue_types_scramble.index(thisBlock['cue'])
+            cue_target_1[0].setLineColor(colors_scramble[color_ind])
+            cue_target_2[0].setLineColor(colors_scramble[color_ind])
+    
+#            if thisBlock['cue']=='target':
+#                cue_target_1[0].setLineColor([-1,1,-1])
+#        
+#                cue_target_2[0].setLineColor([-1,1,-1])
+#            elif thisBlock['cue']=='distractor': # some other color than green. blue?
+#                cue_target_1[0].setLineColor([0,0,1])
+#        
+#                cue_target_2[0].setLineColor([0,0,1])
+#            elif thisBlock['cue']=='neutral': # yellow?
+#                cue_target_1[0].setLineColor([1,1,0])
+#        
+#                cue_target_2[0].setLineColor([1,1,0])
             
             if EEGflag:
                 port.close()
@@ -1052,13 +1221,13 @@ for cue in cue_types_scramble: #looping through the types of cues in sequence, s
     
             draw_fixation()
     
-            target_loc1 = list(stimuli[:6]).index(cue_target_1[0]) #where are the two cued circles?
+            target_loc1 = list(right_stim).index(cue_target_1[0]) #where are the two cued circles?
     
-            stim_minus_one = np.delete(stimuli[:6], target_loc1) 
+            stim_minus_one = np.delete(right_stim, target_loc1) 
     
-            target_loc2 = list(stimuli[6:]).index(cue_target_2[0]) 
+            target_loc2 = list(left_stim).index(cue_target_2[0]) 
     
-            stim_minus_two = np.delete(stimuli[6:],target_loc2) #get a list of stimuli that don't include cued circles
+            stim_minus_two = np.delete(left_stim,target_loc2) #get a list of stimuli that don't include cued circles
     
             stim_minus_both=list(stim_minus_one)+list(stim_minus_two)
     
@@ -1187,9 +1356,9 @@ for cue in cue_types_scramble: #looping through the types of cues in sequence, s
     
             elif thisBlock['cue']=='neutral':
     
-                probe1=np.random.choice(stimuli[:6],1,replace=False) #select two circles, one on left and one on right
+                probe1=np.random.choice(right_stim,1,replace=False) #select two circles, one on left and one on right
     
-                probe2=np.random.choice(stimuli[6:],1,replace=False)
+                probe2=np.random.choice(left_stim,1,replace=False)
     
                 tarNdist=np.random.choice([probe1[0],probe2[0]],2,replace=False) #them randomly assign the target to one and dist to another
     
@@ -1210,8 +1379,11 @@ for cue in cue_types_scramble: #looping through the types of cues in sequence, s
             distractor_stim.ori= (np.random.choice([0,90,180,270],1))[0] #choose the orientation of the distractor
     
             distractor_stim.draw()
-    
-            target_stim.ori= (np.random.choice([0,90,180,270],1))[0] #choose the orientation of the target
+            
+            target_stim.ori=5
+            
+            while target_stim.ori==5 or (target_stim.ori == distractor_stim.ori) or (target_stim.ori+180 == distractor_stim.ori) or (target_stim.ori-180==distractor_stim.ori):
+                target_stim.ori= (np.random.choice([0,90,180,270],1))[0] #choose the orientation of the target
     
             target_stim.draw()
     
